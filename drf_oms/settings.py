6e523525b18bb294e53fb06c9cd1d00a55d67a58
JunHwanLee <jhlee@puzzlesystems.co.kr>
2020-06-03 01:01:24 +0900

    * static_root 추가

--- drf_oms/settings.py
+++ drf_oms/settings.py
@@ -28,7 +28,6 @@ DEBUG = True
 ALLOWED_HOSTS = ['*']
 
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -130,4 +129,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
