b291382d3aeff522eee5ac04717e162fe362a0e1
JunHwanLee <jhlee@puzzlesystems.co.kr>
2020-06-03 01:19:21 +0900

    * installed apps 순서 변경

--- drf_oms/settings.py
+++ drf_oms/settings.py
@@ -38,9 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'knox'
+    'knox',
     'accounts',
-    'orders',
+    'orders'
 ]
 
 AUTH_USER_MODEL = 'accounts.User'
